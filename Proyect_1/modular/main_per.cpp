eb3f0b11ece12a2e2e64c4f20c93654d5b1ccb04
dfuentesc <dfuentesc@unal.edu.co>
2025-06-03 15:08:43 -0500

    proyecto

--- Proyect_1/modular/main_per.cpp
+++ Proyect_1/modular/main_per.cpp
@@ -9,17 +9,17 @@
 int main(int argc, char **argv) {
     const auto seed = std::chrono::high_resolution_clock::now()
                      .time_since_epoch().count();
-    const int nsamples = std::atoi(argv[1]);
+    const long int nsamples = std::atoi(argv[1]);
     const double tes1 = std::atol(argv[2]);
     double tes = tes1/100.0;
     std::mt19937 gen(seed);
     std::uniform_real_distribution<double> dis(0, 1);
-    const int NX = nsamples;
+    const long int NX = nsamples;
     double x[NX][NX]; // Matriz de los valores aleatorios
-    bool grid[NX][NX];  // matriz llena de 0 o 1 
     int cont;
 
     int** matriz_dinamica = new int*[NX]; // Arreglo 2D dinámico
+    
     for (int i = 0; i < NX; ++i) {
         matriz_dinamica[i] = new int[NX];
         for (int j = 0; j < NX; ++j) {
@@ -27,73 +27,33 @@ int main(int argc, char **argv) {
         }
     }
 
-    // Convertir a vector<vector<int>> para usar en la función
-
-
-
-
     for(int n = 0; n < nsamples; ++n) {
         for (int m = 0; m < nsamples; ++m){
             x[n][m]= dis(gen);
-        }
-    }
-
-
-    for(int n = 0; n < nsamples; ++n) {
-        std::cout << "\n" ;
-        for (int m = 0; m < nsamples; ++m){
             if (x[n][m] >= tes){
                 matriz_dinamica[n][m] = 0;
-                std::cout << "\033[48;5;208m  \033[0m"; 
             }
             else{
                 matriz_dinamica[n][m] = 1;
-                std::cout << "\033[44m  \033[0m";
-                //std::cout << "\033[42m  \033[0m";  // Fondo verde
-                
+
             }
         }
     }
 
-    std::cout << "\n";
-
- /*   for(int n = 0; n < nsamples; ++n) {
-        std::cout << "\n" ;
-        for (int m = 0; m < nsamples; ++m){
-            std::cout<< matriz_dinamica[n][m]<< "  " ;
-        }
-    }
-    std::cout << "\n";
-*/
     std::vector<std::vector<int>> matriz_vector(NX, std::vector<int>(NX));
     for (int i = 0; i < NX; ++i) {
         for (int j = 0; j < NX; ++j) {
             matriz_vector[i][j] = matriz_dinamica[i][j];
         }
     }
-    std::cout << (percola(matriz_vector, cont) ? "Percola" : "No percola") << "\n";
-    std::cout << cont << "\n";
+    percola(matriz_vector, cont);
 
-     for(int n = 0; n < nsamples; ++n) {
-        std::cout << "\n" ;
+    for(int n = 0; n < nsamples; ++n) {
         for (int m = 0; m < nsamples; ++m){
-           // std::cout<< matriz_vector[n][m]<< "  " ;
-            switch (matriz_vector[n][m]) {
-                case 1:
-                    std::cout << "\033[44m  \033[0m";
-                    break;
-                case 2:
-                    std::cout << "\033[42m  \033[0m";
-                    break;
-                case 0:
-                    std::cout << "\033[48;5;208m  \033[0m";
-                    break;
-                default:
-                    std::cout << "\033[48;5;208m  \033[0m";
-            }
+            std::cout<< matriz_vector[n][m]<< "  " ;
         }
+        std::cout << "\n" ;
     }
     std::cout << "\n";
-
     return 0;
 }
